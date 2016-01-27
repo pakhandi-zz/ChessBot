7cec6a41970c987f9fd49a4df60a1b41271c930f
pakhandi <asimkprasad@gmail.com>
2016-01-27 20:49:29 +0530

    CleanUp

--- chess_board.py
+++ chess_board.py
@@ -35,57 +35,57 @@ for i in corners:
 	vertices.append((x,y))
 
 #Variables to store four corners of the board
-b_l_x = 10000
-b_l_y = 0
+bottom_left_x = 10000
+bottom_left_y = 0
 
-t_l_x = 10000
-t_l_y = 10000
+top_left_x = 10000
+top_left_y = 10000
 
-b_r_x = 0
-b_r_y = 0
+bottom_right_x = 0
+bottom_right_y = 0
 
-t_r_x = 0
-t_r_y = 10000
+top_right_x = 0
+top_right_y = 10000
 
 minnx = 10000007
 
 #Detecting the four corners of the board
 for point in vertices:
-	if point[0] <= t_l_x and point[1] <= t_l_y:
-		t_l_x,t_l_y = point[0],point[1]
-	if point[0] <= b_l_x and point[1] >= b_l_y:
-		b_l_x,b_l_y = point[0],point[1]
-	if point[0] >= b_r_x and point[1] >= b_r_y:
-		b_r_x,b_r_y = point[0],point[1]
-	if point[0] >= t_r_x and point[1] <= t_r_y:
+	if point[0] <= top_left_x and point[1] <= top_left_y:
+		top_left_x,top_left_y = point[0],point[1]
+	if point[0] <= bottom_left_x and point[1] >= bottom_left_y:
+		bottom_left_x,bottom_left_y = point[0],point[1]
+	if point[0] >= bottom_right_x and point[1] >= bottom_right_y:
+		bottom_right_x,bottom_right_y = point[0],point[1]
+	if point[0] >= top_right_x and point[1] <= top_right_y:
 		print point[0]," ",point[1]
-		t_r_x,t_r_y = point[0],point[1]
+		top_right_x,top_right_y = point[0],point[1]
 
 #Marking the four corners of the board
-cv2.circle(img,(t_l_x,t_l_y),1,255,-1)
-cv2.circle(img,(b_l_x,b_l_y),1,255,-1)
-cv2.circle(img,(b_r_x,b_r_y),1,255,-1)
-cv2.circle(img,(t_r_x,t_r_y),1,255,-1)
+cv2.circle(img,(top_left_x,top_left_y),1,255,-1)
+cv2.circle(img,(bottom_left_x,bottom_left_y),1,255,-1)
+cv2.circle(img,(bottom_right_x,bottom_right_y),1,255,-1)
+cv2.circle(img,(top_right_x,top_right_y),1,255,-1)
 
 #Making four sides around the board
-cv2.line(img, (t_l_x,t_l_y), (t_r_x,t_r_y), GREEN, 2)
-cv2.line(img, (t_l_x,t_l_y), (b_l_x,b_l_y), GREEN, 2)
-cv2.line(img, (b_l_x,b_l_y), (b_r_x,b_r_y), GREEN, 2)
-cv2.line(img, (b_r_x,b_r_y), (t_r_x,t_r_y), GREEN, 2)
+cv2.line(img, (top_left_x,top_left_y), (top_right_x,top_right_y), GREEN, 2)
+cv2.line(img, (top_left_x,top_left_y), (bottom_left_x,bottom_left_y), GREEN, 2)
+cv2.line(img, (bottom_left_x,bottom_left_y), (bottom_right_x,bottom_right_y), GREEN, 2)
+cv2.line(img, (bottom_right_x,bottom_right_y), (top_right_x,top_right_y), GREEN, 2)
 
 #Calculating the points on all the four sides of the boards
 # ------------TOP Side------------------------
 
 top_line = []
-top_line.append( (t_l_x,t_l_y) )
-top_line_length = get_dist( (t_l_x, t_l_y) , (t_r_x,t_r_y))
+top_line.append( (top_left_x,top_left_y) )
+top_line_length = get_dist( (top_left_x, top_left_y) , (top_right_x,top_right_y))
 top_unit_length = top_line_length / 3
 
 
 for i in range(0,2):
-	top_line.append( (t_l_x + ( top_unit_length * (i+1) ) , t_l_y ) )
+	top_line.append( (top_left_x + ( top_unit_length * (i+1) ) , top_left_y ) )
 
-top_line.append( (t_r_x, t_r_y ) )
+top_line.append( (top_right_x, top_right_y ) )
 
 for point in top_line:
 	cv2.circle(img,(point[0],point[1]), 1, BLUE, -1)
@@ -93,14 +93,14 @@ for point in top_line:
 # -------------BOTTOM Side---------------------------------------
 
 bottom_line = []
-bottom_line.append( (b_l_x,b_l_y) )
-bottom_line_length = get_dist( (b_l_x,b_l_y) , (b_r_x, b_r_y))
+bottom_line.append( (bottom_left_x,bottom_left_y) )
+bottom_line_length = get_dist( (bottom_left_x,bottom_left_y) , (bottom_right_x, bottom_right_y))
 bottom_unit_length = bottom_line_length / 3
 
 for i in range(0,2):
-	bottom_line.append( (b_l_x + ( bottom_unit_length * (i+1) ) , b_l_y ) )
+	bottom_line.append( (bottom_left_x + ( bottom_unit_length * (i+1) ) , bottom_left_y ) )
 
-bottom_line.append( (b_r_x, b_r_y) )
+bottom_line.append( (bottom_right_x, bottom_right_y) )
 
 for point in bottom_line:
 	cv2.circle(img, (int(point[0]),int(point[1])) , 1, BLUE, -1)
@@ -108,14 +108,14 @@ for point in bottom_line:
 # -------------LEFT Side--------------------------------------------
 
 left_line = []
-left_line.append( (t_l_x,t_l_y) )
-left_line_length = get_dist( (t_l_x,t_l_y) , (b_l_x, b_l_y))
+left_line.append( (top_left_x,top_left_y) )
+left_line_length = get_dist( (top_left_x,top_left_y) , (bottom_left_x, bottom_left_y))
 left_unit_length = left_line_length / 3
 
 for i in range(0,2):
-	left_line.append( (t_l_x , t_l_y + ( (i+1) * left_unit_length ) ) );
+	left_line.append( (top_left_x , top_left_y + ( (i+1) * left_unit_length ) ) );
 
-left_line.append( (b_l_x, b_l_y) )
+left_line.append( (bottom_left_x, bottom_left_y) )
 
 for point in left_line:
 	cv2.circle(img, (int(point[0]),int(point[1])) , 1, BLUE, -1)
@@ -123,14 +123,14 @@ for point in left_line:
 # -------------RIGHT Side----------------------------------------------
 
 right_line = []
-right_line.append( (t_r_x,t_r_y) )
-right_line_length = get_dist( (t_r_x,t_r_y) , (b_r_x, b_r_y))
+right_line.append( (top_right_x,top_right_y) )
+right_line_length = get_dist( (top_right_x,top_right_y) , (bottom_right_x, bottom_right_y))
 right_unit_length = right_line_length / 3
 
 for i in range(0,2):
-	right_line.append( (t_r_x , t_r_y + ( (i+1) * right_unit_length ) ) );
+	right_line.append( (top_right_x , top_right_y + ( (i+1) * right_unit_length ) ) );
 
-right_line.append( (b_r_x, b_r_y) )
+right_line.append( (bottom_right_x, bottom_right_y) )
 
 for point in right_line:
 	cv2.circle(img, (int(point[0]),int(point[1])) , 1, BLUE, -1)
@@ -165,7 +165,7 @@ for i in corners:
 matrix = [[(0,0) for x in range(5)] for x in range(5)]
 
 #Filling the matrix
-matrix[0][0] = (t_l_x,t_l_y)
+matrix[0][0] = (top_left_x,top_left_y)
 
 for i in range(0,4):
 	for j in range(0,4):
