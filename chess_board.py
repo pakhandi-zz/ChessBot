fbdf4d5bf78d1058a3933b826072dc5095fc362b
pakhandi <asimkprasad@gmail.com>
2016-01-28 12:14:10 +0530

    Temp Directory

--- chess_board.py
+++ chess_board.py
@@ -10,7 +10,6 @@ import cv2
 import sys
 from matplotlib import pyplot as plt
 from math import *
-import Image, ImageDraw
 
 # Global Constants
 # ---------------------------------------------------------------------------
--- sharpen.py
+++ sharpen.py
@@ -4,6 +4,8 @@
 import cv2
 import numpy as np
 import sys
+import os
+import shutil
 
 #Load source / input image as grayscale, also works on color images...
 imgIn = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
@@ -19,7 +21,10 @@ boxFilter = np.ones( (9,9), np.float32) / 81.0
 kernel = kernel - boxFilter
 
 custom = cv2.filter2D(imgIn, -1, kernel)
-
 cv2.imwrite("sharp.jpg", custom)
+
+if not os.path.exists("temp/"):
+	os.makedirs("temp/")
+shutil.move("sharp.jpg","temp/sharp.jpg")
 
 exit()
