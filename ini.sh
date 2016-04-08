7a7adaa4c5c99b4ca09a675c313062558c86d3ff
pakhandi <asimkprasad@gmail.com>
2016-04-08 23:16:21 +0530

    Update driver script

--- ini.sh
+++ ini.sh
@@ -8,11 +8,11 @@ garbage=1
 while true; do
 	read garbage
 	python camera.py $i
-	# python test.py img/blankBoard.jpg img/$i.jpg 200 > thisPlayerMatrix
-	# ./rotateBaord > tempFile
-	# cat tempFile > thisPlayerMatrix
-	# ./generateBoard < thisPlayerMatrix > tempFile
-	# cat tempFile > prevBoard
-	# ./nextMove < prevBoard
+	python test.py img/blankBoard.jpg img/$i.jpg 200 > thisPlayerMatrix
+	./rotateBaord > tempFile
+	cat tempFile > thisPlayerMatrix
+	./generateBoard < thisPlayerMatrix > tempFile
+	cat tempFile > prevBoard
+	./nextMove < prevBoard
 	i=$((i + 1))
 done
