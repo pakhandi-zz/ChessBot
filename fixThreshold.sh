40192f8c0399c3b60f44f5757f6312ace2c7854b
pakhandi <asimkprasad@gmail.com>
2016-04-11 22:01:56 +0530

    Enhancements

--- fixThreshold.sh
+++ fixThreshold.sh
@@ -1,10 +1,29 @@
 #! /usr/bin/bash
 
-i=30
+i=60
 garbage=1
+down=0
+up=60
 
-# python camera.py blankBoard
-# python camera.py fix
+# python camera.py blankBoard 1
+# python camera.py fix 1
+
+while true; do
+	python test.py img/blankBoard.jpg img/fix.jpg 200 $i > thisPlayerMatrix.txt
+	cat thisPlayerMatrix.txt
+	echo "=============================="
+	./checkBlack
+
+	if [ $? -eq 0 ]
+	then
+		# echo "here"
+		up=$i
+		break
+	fi
+	i=$((i - 1))
+done
+
+i=0
 
 while true; do
 	python test.py img/blankBoard.jpg img/fix.jpg 200 $i > thisPlayerMatrix.txt
@@ -14,8 +33,10 @@ while true; do
 	if [ $? -eq 0 ]
 	then
 		# echo "here"
-		echo $i > tempFile.txt
-		exit
+		down=$i
+		break
 	fi
-	i=$((i - 1))
+	i=$((i + 1))
 done
+
+echo $((up - 1))
