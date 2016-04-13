b30bb635832be898b4b54383988c34cf7834a55e
Asim Krishna Prasad <pakhandi@users.noreply.github.com>
2016-04-13 09:51:41 +0530

    Add arduino support

---
+++ sketch_apr12a.ino
@@ -0,0 +1,17 @@
+#include<SoftwareSerial.h>
+SoftwareSerial esp(0,1);
+char c;
+void setup() {
+  // put your setup code here, to run once:
+esp.begin(9600);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(esp.available()>0){
+  c=esp.read();
+  Serial.print(c);}
+  //if(c>='0'&&c<='9')
+    //  Serial.print(c);
+}
