064d108f0e2cef1c7843d21603a18f2e3aafa31c
pakhandi <asimkprasad@gmail.com>
2016-04-13 06:59:02 +0530

    Add bluetooth support

--- blue.py
+++ blue.py
@@ -7,9 +7,9 @@ port = 1
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 while 1:
-    text = 'F' # Note change to the old (Python 2) raw_input
-    if text == "quit":
-		break
+  #   text = 'F' # Note change to the old (Python 2) raw_input
+  #   if text == "quit":
+		# break
     s.send(sys.argv[1])
     break
 s.close()
