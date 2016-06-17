8d857f6cb3eef172e1b935c16fa97cbe82ef4586
pakhandi <asimkprasad@gmail.com>
2016-06-17 19:32:10 +0530

    Update fixthreshold

--- fixThreshold.sh
+++ fixThreshold.sh
@@ -3,10 +3,10 @@
 i=60
 garbage=1
 down=0
-up=60
+up=50
 
-python camera.py blankBoard 1
-python camera.py fix 1
+# python camera.py blankBoard 1
+# python camera.py fix 1
 
 
 while true; do
@@ -23,20 +23,22 @@ while true; do
 	i=$((i - 1))
 done
 
-# i=0
+i=0
 
-# while true; do
-# 	python test.py img/blankBoard.jpg img/fix.jpg 600 $i > thisPlayerMatrix.txt
-# 	echo $i
-# 	cat thisPlayerMatrix.txt
-# 	echo "=============================="
-# 	./checkBlack
-# 	if [ $? -eq 0 ]
-# 	then
-# 		down=$i
-# 		break
-# 	fi
-# 	i=$((i + 1))
-# done
+while true; do
+	python test.py img/blankBoard.jpg img/fix.jpg 600 $i > thisPlayerMatrix.txt
+	echo $i
+	cat thisPlayerMatrix.txt
+	echo "=============================="
+	./checkBlack < thisPlayerMatrix.txt
+	if [ $? -eq 0 ]
+	then
+		down=$i
+		break
+	fi
+	i=$((i + 1))
+done
 
-echo $((up - 1)) > thresholdVal.txt
+echo $down
+
+echo $((down)) > thresholdVal.txt
