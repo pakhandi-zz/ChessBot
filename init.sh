6c64192fb7bc63c153022c11b6b7cc895215af97
pakhandi <asimkprasad@gmail.com>
2016-06-25 18:43:15 +0530

    Add comments

--- init.sh
+++ init.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/bash
 
+'''
+
+	This is where the program starts
+	This script is the driver and does following things :
+	1> fixThreshold
+	2> initialize the prevBoard.txt
+	3> run play.sh
+
+'''
+
 sh fixThreshold.sh
 
 cat initBoard.txt > prevBoard.txt
