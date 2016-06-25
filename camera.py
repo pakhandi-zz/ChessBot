bcb4211e23a5bc935e8e3871cae5a229c229dbf6
pakhandi <asimkprasad@gmail.com>
2016-06-25 18:52:48 +0530

    Add comments and clean camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,10 @@
 '''
 	Program to get live stream from secondary web cam and click a picture on press of a button
+	This script performs following tasks:
+	1> The first argument is the filename for the capture
+		1.1> .jpg is appended at the end of the filename
+	2> The second argument decides if the script should wait for a key press or capture the shot as soon as the script is run
+	3> The images are saved in the TARGET_DIRECTORY directory
 '''
 import numpy as np
 import cv2
@@ -7,6 +12,7 @@ import sys
 import os
 import shutil
 
+TARGET_DIRECTORY = "img/"
 
 # Checking arguments
 if len(sys.argv) != 3:
@@ -23,26 +29,23 @@ while(True):
 	# Capture frame-by-frame
 	ret, frame = cap.read()
 
-	# Our operations on the frame come here
-	#gray = cv2.cvtColor(frame, cv2.COLOR)
-
 	# Display the resulting frame
 	cv2.imshow('frame',frame)
 
 	# for auto click
 	if sys.argv[2] == '0':
 		cv2.imwrite(filename,frame)
-		if not os.path.exists("img/"):
-			os.makedirs("img/")
-		shutil.move(filename,"img/"+filename)
+		if not os.path.exists(TARGET_DIRECTORY):
+			os.makedirs(TARGET_DIRECTORY)
+		shutil.move(filename,TARGET_DIRECTORY+filename)
 		break
 
-	#Save the frame and quit on 'q'
+	# Save the frame and quit on 'q'
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		cv2.imwrite(filename,frame)
-		if not os.path.exists("img/"):
-			os.makedirs("img/")
-		shutil.move(filename,"img/"+filename)
+		if not os.path.exists(TARGET_DIRECTORY):
+			os.makedirs(TARGET_DIRECTORY)
+		shutil.move(filename,TARGET_DIRECTORY+filename)
 		break
 
 # When everything done, release the capture
